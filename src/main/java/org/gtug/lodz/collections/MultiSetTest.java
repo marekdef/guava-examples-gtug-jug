edc865c9d7a0c86239868aaf0aba93812b2b9c18
Marek Defeciński <marekdef@gmail.com>
2012-04-25 21:21:25 +0200

    Comments for MultiSet tests

--- src/main/java/org/gtug/lodz/collections/MultiSetTest.java
+++ src/main/java/org/gtug/lodz/collections/MultiSetTest.java
@@ -16,17 +16,22 @@ public class MultiSetTest {
 	@Test
 	public void testMultiSetCounts() {
 		// given
-		// when
+		// when we add symbols
 		words.add("GTUG");
+		// several times
 		words.add("GTUG", 5);
+		// remove symbols
 		words.add("JUG");
+		// even more times they are inserted
 		words.remove("JUG", 2);
 
-		// then
+		// then we get correct count on multiple inserts
 		assertThat(words.count("GTUG")).isEqualTo(6);
+		// removed word does not get negative count
 		assertThat(words.count("JUG")).isEqualTo(0);
+		// never present symbols are counted as 0
 		assertThat(words.count("Lodz")).isEqualTo(0);
-
+		// only one represented keys are present
 		assertThat(words.elementSet()).containsOnly("GTUG");
 	}
 
@@ -34,25 +39,25 @@ public class MultiSetTest {
 	public void testMultiSetSetCount() {
 		// given
 		// when
-		words.add("GTUG", 5);
+		words.add("GTUG", 2);
 		words.add("JUG");
-		words.remove("JUG", 2);
-		words.setCount("GTUG", 5);
-		words.setCount("JUG", 0);
+		
+		// using setCount to set count 
+		words.setCount("GTUG", 0);
+		words.setCount("JUG", 5);
 
-		// then
-		assertThat(words.count("GTUG")).isEqualTo(5);
-		assertThat(words.count("JUG")).isEqualTo(0);
-		assertThat(words.count("Lodz")).isEqualTo(0);
+		// then we get correct counts
+		assertThat(words.count("GTUG")).isEqualTo(0);
+		assertThat(words.count("JUG")).isEqualTo(5);
 
-		assertThat(words.elementSet()).containsOnly("GTUG");
+		assertThat(words.elementSet()).containsOnly("JUG");
 	}
 
 	@Test
 	public void testMultiSetSetCountInvalid() {
 		// given
 		thrown.expect(IllegalArgumentException.class);
-		// when
+		// when inserting illegal count
 		words.setCount("Illegal", -1);
 		// then
 	}
