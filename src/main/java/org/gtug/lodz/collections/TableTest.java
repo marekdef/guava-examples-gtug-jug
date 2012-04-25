8be763cbd0c1541a440671ee362ff8794d803c76
Marek Defeciński <marekdef@gmail.com>
2012-04-25 21:30:32 +0200

    Comments to Table test explaining manipulation on view

--- src/main/java/org/gtug/lodz/collections/TableTest.java
+++ src/main/java/org/gtug/lodz/collections/TableTest.java
@@ -13,18 +13,22 @@ public class TableTest {
 
 	@Test
 	public void testTableCreation() {
-		// given
+		// given a table
 		Table<String, String, Double> distance = TreeBasedTable.create();
 
-		// when
+		// when filling data
 		distance.put("Barcelona", "Berlin", 1497.61);
 		distance.put("Barcelona", "Warsaw", 1862.33);
 		distance.put("Barcelona", "Madrid", 504.64);
 
+		// obtaining a row
 		Map<String, Double> barcelonaRow = distance.row("Barcelona");
 
-		// then
+		// then we can query rows
 		assertThat(barcelonaRow).hasSize(3);
+		// for elements
+		assertThat(barcelonaRow.get("Warsaw")).isEqualTo(1862.33);
+		// and a table directly
 		assertThat(distance.get("Barcelona", "Berlin")).isEqualTo(1497.61);
 	}
 
@@ -34,7 +38,7 @@ public class TableTest {
 		Table<String, String, Double> distance = TreeBasedTable.create();
 		// when
 
-		// then
+		// then we get not null rows for not existing rows
 		assertThat(distance.row("Warsaw")).isNotNull();
 		assertThat(distance.column("Barcelona")).isNotNull();
 	}
@@ -49,22 +53,29 @@ public class TableTest {
 		distance.put("Barcelona", "Warsaw", 1862.33);
 		distance.put("Barcelona", "Madrid", 504.64);
 
+		// we obtain a row (view)
 		Map<String, Double> londonRow = distance.row("London");
+		// update table directly
 		distance.put("London", "Warsaw", 1445.85);
+		// update using view
 		londonRow.put("Istambul", 2496.39);
 
 		Map<String, Double> barcelonaRow = distance.row("Barcelona");
 		Map<String, Double> warsawColumn = distance.column("Warsaw");
 		Map<String, Double> madridColumn = distance.column("Madrid");
 
+		// we clear barcelona
 		barcelonaRow.clear();
 
+		// update warsaw View
 		warsawColumn.put("Paris", 1365.91);
 		madridColumn.put("Paris", 1053.40);
 
-		// then
+		// then barcelona is empty
 		assertThat(barcelonaRow).isEmpty();
-		assertThat(warsawColumn).hasSize(2);
+		// warsaw gets connection from London and Paris but not Barcelona
+		assertThat(warsawColumn.keySet()).containsOnly("Paris","London");
+		// london row updated in mixed mode is trully updated 
 		assertThat(londonRow).hasSize(2);
 		assertThat(distance.row("Paris")).hasSize(2);
 		assertThat(distance.get("Paris", "Warsaw")).isEqualTo(1365.91);
